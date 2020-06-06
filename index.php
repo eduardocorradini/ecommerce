46bb061a6305f8797275c53b70c5813951cccaf6
Eduardo Corradini <eduardo_corradini@yahoo.com.br>
2020-06-06 14:04:30 -0300

    admin

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once("vendor/autoload.php");
 
 use Slim\Slim;
 use Hcode\Page;
+use Hcode\PageAdmin;
 
 $app = new Slim();
 
@@ -12,6 +13,11 @@ $app->config('debug', true);
 $app->get('/', function() {
     
 	$page = new Page();
+	$page->setTpl("index",$data = array(1,2));
+});
+$app->get('/admin', function() {
+    
+	$page = new PageAdmin();
 	$page->setTpl("index",$data = array(1,2));
 });
 
